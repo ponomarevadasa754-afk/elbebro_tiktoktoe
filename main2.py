dc9eb9a9dc1c1f3bf3c85375c7dc6e82db9cfec9
elbebro1 <ghhh12360@mail.ru>
2025-10-26 18:12:37 +0300

    сделал поле и ввод и закоммитил

--- main2.py
+++ main2.py
@@ -1,3 +1,24 @@
+def get_player_input(board, current_player):
+    """Получает и проверяет ввод игрока"""
+    while True:
+        try:
+            position = int(input(f"Игрок {current_player}, выбери позицию (1-9): "))
+            if 1 <= position <= 9:
+                if board[position-1] == " ":
+                    return position - 1
+                else:
+                    print("Эта клетка уже занята! Выбери другую.")
+            else:
+                print("Введи число от 1 до 9!")
+        except ValueError:
+            print("Нужно ввести число!")
+def display_board(board):
+    """Отображает игровое поле"""
+    print(f"\n {board[0]} | {board[1]} | {board[2]} ")
+    print("-----------")
+    print(f" {board[3]} | {board[4]} | {board[5]} ")
+    print("-----------")
+    print(f" {board[6]} | {board[7]} | {board[8]} \n")
 def display_rules():
     """Показывает правила игры"""
     print("\n=== ПРАВИЛА ИГРЫ ===")
@@ -13,6 +34,18 @@ def main():
     """Основная функция игры"""
     print("🎮 Добро пожаловать в крестики-нолики! 🎮")
     display_rules()
+    while True:
+        # Инициализация игры
+        board = [" "] * 9
+        current_player = "X"
+        game_over = False
+        
+        while not game_over:
+            display_board(board)
+            
+            # Ход игрока
+            position = get_player_input(board, current_player)
+            board[position] = current_player
 
 
 if __name__ == "__main__":
